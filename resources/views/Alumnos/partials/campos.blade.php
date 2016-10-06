9bc6d551ed56679873b9b656ef4da9f7070f4c06
vagrant <vagrant@homestead>
2016-10-06 00:44:42 +0000

    show/create corregido

--- resources/views/Alumnos/partials/campos.blade.php
+++ resources/views/Alumnos/partials/campos.blade.php
@@ -1,155 +1,13 @@
 <div class="form-group">
- 	<div class="col-lg-3">
- 		{!! Form::label('nombre','Nombre:',['class' => 'control-label'])!!}
- 	</div>
- 	
- 	<div class="col-lg-5">
+	<div class="col-lg-3">
+ 		{!! Form::label('nombre','Nombre(s):',['class' => 'control-label'])!!}
+ 	</div> 	
+	<div class="col-lg-3">
 		{!! Form::text('nombre',null,['class' => 'form-control']) !!}
-	</div>
-</div>
+	</div>	
 
-<div class="form-group">
-<div class="col-lg-3">
- 		{!! Form::label('apa','Apellido Paterno:',['class' => 'control-label'])!!}
- 	</div>
- 	
- 	<div class="col-lg-5">
-		{!! Form::text('apa',null,['class' => 'form-control']) !!}
-	</div>
-</div>
 
-<div class="form-group">
-<div class="col-lg-3">
- 		{!! Form::label('ama','Apellido Materno:',['class' => 'control-label'])!!}
- 	</div>
- 	
- 	<div class="col-lg-5">
-		{!! Form::text('ama',null,['class' => 'form-control']) !!}
-	</div>
-</div>
-
-<div class="form-group">
-<div class="col-lg-3">
- 		{!! Form::label('nacimiento','Fecha de Nacimiento:',['class' => 'control-label'])!!}
- 	</div>
- 	
- 	<div class="col-lg-5">
-		{!! Form::date('nacimiento',null,['class' => 'form-control']) !!}
-	</div>
-</div>
-
-<div class="form-group">
-<div class="col-lg-3">
- 		{!! Form::label('nacionalidad','Nacionalidad:',['class' => 'control-label'])!!}
- 	</div>
- 	
- 	<div class="col-lg-5">
-		{!! Form::text('nacionalidad',null,['class' => 'form-control']) !!}
-	</div>
-</div>
-
-<div class="form-group">
-<div class="col-lg-3">
- 		{!! Form::label('curp','CURP:',['class' => 'control-label'])!!}
- 	</div>
- 	
- 	<div class="col-lg-5">
-		{!! Form::text('curp',null,['class' => 'form-control']) !!}
-	</div>
-</div>
-
-<div class="form-group">
-<div class="col-lg-3">
- 		{!! Form::label('matricula','Matrícula:',['class' => 'control-label'])!!}
- 	</div>
- 	
- 	<div class="col-lg-5">
-		{!! Form::text('matricula',null,['class' => 'form-control']) !!}
-	</div>
-</div>
-
-<div class="form-group">
-<div class="col-lg-3">
- 		{!! Form::label('contraseña','Contraseña:',['class' => 'control-label'])!!}
- 	</div>
- 	
- 	<div class="col-lg-5">
-		{!! Form::password('contraseña',null,['class' => 'form-control']) !!}
-	</div>
-</div>
-
-<div class="form-group">
-<div class="col-lg-3">
- 		{!! Form::label('colonia','Colonia:',['class' => 'control-label'])!!}
- 	</div>
- 	
- 	<div class="col-lg-5">
-		{!! Form::text('colonia',null,['class' => 'form-control']) !!}
-	</div>
-</div>
-
-<div class="form-group">
-<div class="col-lg-3">
- 		{!! Form::label('calle','Calle:',['class' => 'control-label'])!!}
- 	</div>
- 	
- 	<div class="col-lg-5">
-		{!! Form::text('calle',null,['class' => 'form-control']) !!}
-	</div>
-</div>
-
-<div class="form-group">
-<div class="col-lg-3">
- 		{!! Form::label('cp','Código Postal:',['class' => 'control-label'])!!}
- 	</div>
- 	
- 	<div class="col-lg-5">
-		{!! Form::text('cp',null,['class' => 'form-control']) !!}
-	</div>
-</div>
-
-<div class="form-group">
-<div class="col-lg-3">
- 		{!! Form::label('municipio','Municipio:',['class' => 'control-label'])!!}
- 	</div>
- 	
- 	<div class="col-lg-5">
-		{!! Form::text('municipio',null,['class' => 'form-control']) !!}
-	</div>
-</div>
-
-<div class="form-group">
-<div class="col-lg-3">
- 		{!! Form::label('estado','Estado:',['class' => 'control-label'])!!}
- 	</div>
- 	
- 	<div class="col-lg-5">
-		{!! Form::text('estado',null,['class' => 'form-control']) !!}
-	</div>
-</div>
-
-<div class="form-group">
-<div class="col-lg-3">
- 		{!! Form::label('telefono','Teléfono:',['class' => 'control-label'])!!}
- 	</div>
- 	
- 	<div class="col-lg-5">
-		{!! Form::text('telefono',null,['class' => 'form-control']) !!}
-	</div>
-</div>
-
-<div class="form-group">
-<div class="col-lg-3">
- 		{!! Form::label('correo','Correo:',['class' => 'control-label'])!!}
- 	</div>
- 	
- 	<div class="col-lg-5">
-		{!! Form::text('correo',null,['class' => 'form-control']) !!}
-	</div>
-</div>
-
-<div class="form-group">
-<div class="col-lg-3">
+	<div class="col-lg-1">
  		{!! Form::label('foto','Foto:',['class' => 'control-label'])!!}
  	</div>
  	
@@ -160,10 +18,143 @@
 
 <div class="form-group">
 	<div class="col-lg-3">
+ 		{!! Form::label('apa','Apellido Paterno:',['class' => 'control-label'])!!}
+ 	</div> 	
+	<div class="col-lg-3">
+		{!! Form::text('apa',null,['class' => 'form-control']) !!}
+	</div>
+
+
+	<div class="col-lg-3"> 
+		{!! Form::label('ama','Apellido Materno:',['class' => 'control-label'])!!}
+ 	</div> 	
+
+ 	<div class="col-lg-3">
+		{!! Form::text('ama',null,['class' => 'form-control']) !!}
+	</div>
+</div>
+
+<div class="form-group">
+	<div class="col-lg-3">
+ 		{!! Form::label('nacimiento','Fecha de Nacimiento:',['class' => 'control-label'])!!}
+ 	</div>
+ 	
+ 	<div class="col-lg-3">
+		{!! Form::date('nacimiento',null,['class' => 'form-control']) !!}
+	</div>
+</div>
+
+<div class="form-group">
+	<div class="col-lg-3">
+ 		{!! Form::label('nacionalidad','Nacionalidad:',['class' => 'control-label'])!!}
+ 	</div>
+ 	
+ 	<div class="col-lg-3">
+		{!! Form::text('nacionalidad',null,['class' => 'form-control']) !!}
+	</div>
+</div>
+
+<div class="form-group">
+	<div class="col-lg-3">
+ 		{!! Form::label('curp','CURP:',['class' => 'control-label'])!!}
+ 	</div>
+ 	
+ 	<div class="col-lg-3">
+		{!! Form::text('curp',null,['class' => 'form-control']) !!}
+	</div>
+</div>
+
+<div class="form-group">
+	<div class="col-lg-3">
+ 		{!! Form::label('matricula','Matrícula:',['class' => 'control-label'])!!}
+ 	</div>
+ 	
+ 	<div class="col-lg-3">
+		{!! Form::text('matricula',null,['class' => 'form-control']) !!}
+	</div>
+
+
+	<div class="col-lg-2">
+ 		{!! Form::label('contraseña','Contraseña:',['class' => 'control-label'])!!}
+ 	</div>
+ 	
+ 	<div class="col-lg-3">
+		{!! Form::password('contraseña',null,['class' => 'form-control']) !!}
+	</div>
+</div>
+
+<div class="form-group">
+	<div class="col-lg-3">
+ 		{!! Form::label('colonia','Colonia:',['class' => 'control-label'])!!}
+ 	</div>
+ 	
+ 	<div class="col-lg-3">
+		{!! Form::text('colonia',null,['class' => 'form-control']) !!}
+	</div>
+
+
+	<div class="col-lg-2">
+ 		{!! Form::label('calle','Calle:',['class' => 'control-label'])!!}
+ 	</div>
+ 	
+ 	<div class="col-lg-4">
+		{!! Form::text('calle',null,['class' => 'form-control']) !!}
+	</div>
+</div>
+
+<div class="form-group">
+<div class="col-lg-2">
+ 		{!! Form::label('cp','Código Postal:',['class' => 'control-label'])!!}
+ 	</div>
+ 	
+ 	<div class="col-lg-2">
+		{!! Form::text('cp',null,['class' => 'form-control']) !!}
+	</div>
+
+
+	<div class="col-lg-1">
+ 		{!! Form::label('municipio','Municipio:',['class' => 'control-label'])!!}
+ 	</div>
+ 	
+ 	<div class="col-lg-3">
+		{!! Form::text('municipio',null,['class' => 'form-control']) !!}
+	</div>
+
+
+	<div class="col-lg-1">
+ 		{!! Form::label('estado','Estado:',['class' => 'control-label'])!!}
+ 	</div>
+ 	
+ 	<div class="col-lg-3">
+		{!! Form::text('estado',null,['class' => 'form-control']) !!}
+	</div>
+</div>
+
+<div class="form-group">
+	<div class="col-lg-2">
+ 		{!! Form::label('telefono','Teléfono:',['class' => 'control-label'])!!}
+ 	</div>
+ 	
+ 	<div class="col-lg-3">
+		{!! Form::text('telefono',null,['class' => 'form-control']) !!}
+	</div>
+
+
+	<div class="col-lg-2">
+ 		{!! Form::label('correo','Correo:',['class' => 'control-label'])!!}
+ 	</div>
+ 	
+ 	<div class="col-lg-5">
+		{!! Form::text('correo',null,['class' => 'form-control']) !!}
+	</div>
+</div>
+
+<div class="form-group">
+	<div class="col-lg-3">
  		{!! Form::label('nombre','Selecciona un Grupo:',['class' => 'control-label'])!!}
  	</div>
 
-	<div class="col-lg-5">
+	<div class="col-lg-2">
 		{!! Form::select('grupos_id', $Grupos, null, array('class' => 'form-control')) !!}
 	</div>
 </div>
---
+++ resources/views/Reportes/index.blade.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<?php
+	$mensaje = Session::get('message')
+?>
+
+<head>
+	<meta charset="UTF-8">
+
+	@extends('layouts.master')
+    
+	<title>Sistema de Control Escolar</title>
+</head>
+<body>
+
+@section('content2')
+
+@include('layouts.partials.menuGD')
+
+	@if($mensaje == 'store')
+		<div class="alert alert-warning alert-dismissible" role="alert">
+  			<button type="button" class="close" data-dismiss="alert" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+  		<p>Acción realizada con exito</p>
+		</div>
+	@endif
+	
+@endsection
+	
+</body>
+</html>
